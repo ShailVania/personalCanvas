bb1cd85119bba3764c5c21d92aadda5b1eef2ab6
Shail Vania <vaniashail@gmail.com>
2025-06-25 14:42:20 +0000

    make the content of the page to be aligned and justified to center for b

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -7,7 +7,9 @@ export function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="container mx-auto flex-1 px-6 py-8">{children}</main>
+      <main className="container mx-auto flex flex-1 flex-col items-center justify-center px-6 py-8">
+        {children}
+      </main>
       <Footer />
     </div>
   );
