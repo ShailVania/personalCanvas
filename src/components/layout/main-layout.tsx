64f8fea9c8fa502072c8786ed08e48c4a927351a
Shail Vania <vaniashail@gmail.com>
2025-06-25 12:49:52 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="space-

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -7,7 +7,7 @@ export function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="flex-1 container py-8">{children}</main>
+      <main className="flex-1 container py-8 px-6">{children}</main>
       <Footer />
     </div>
   );
