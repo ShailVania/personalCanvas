bb04b5017a92e701f03193f379b002801a35de23
Shail Vania <vaniashail@gmail.com>
2025-06-25 13:07:59 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Footer>
    
    with the

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,7 +5,7 @@ import { Button } from '../ui/button';
 export function Footer() {
   return (
     <footer className="w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex flex-col items-center justify-between gap-4 px-6 py-10 md:h-24 md:flex-row md:py-0">
+      <div className="container flex flex-col items-center justify-between gap-4 px-6 py-6 md:h-16 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 md:flex-row md:gap-3">
           <Code className="h-8 w-8" />
           <p className="text-center text-sm leading-loose md:text-left">
