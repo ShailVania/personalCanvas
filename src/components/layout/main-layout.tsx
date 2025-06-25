81cc52eb3ac30a9d1cf2b1b69cab7e59b1716b24
Shail Vania <vaniashail@gmail.com>
2025-06-25 12:55:53 +0000

    make all the content on the pages to be at the center, while in full scr

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -7,7 +7,7 @@ export function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="flex-1 container py-8 px-6">{children}</main>
+      <main className="container mx-auto flex-1 px-6 py-8">{children}</main>
       <Footer />
     </div>
   );
