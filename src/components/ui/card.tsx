864f60193c9afa8f138aabbb02e54b9d61e7197a
Shail Vania <vaniashail@gmail.com>
2025-06-27 09:27:30 +0000

    make it more blur

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-xl border border-border/20 bg-card/50 text-card-foreground shadow-lg backdrop-blur-lg",
+      "rounded-xl border border-border/20 bg-card/50 text-card-foreground shadow-lg backdrop-blur-xl",
       className
     )}
     {...props}
